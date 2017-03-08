ebdf180036084e44e2b615d1920371fb9ce7d197
Leon Weber <leonweber@posteo.de>
2017-03-08 10:57:53 +0100

    fix error with entitites containing delimiter

--- drop_low_score_entities.py
+++ drop_low_score_entities.py
@@ -23,29 +23,25 @@ def collect_confidences():
     return np.array(confidences)
 
 confidences = collect_confidences()
-lowest_confidence_allowed = np.percentile(confidences, args.percent_dropped)
+confidence_threshold = np.percentile(confidences, args.percent_dropped)
 
 def write_to_conll():
     with open(args.out, 'w') as f_out, open(args.tagged) as f_tagged,\
             open(args.true) as f_true:
         for tagged_line, true_line in zip(f_tagged, f_true):
-            confidence = None
             try:
                 true_tag, token, pos_tag = true_line.split()
             except ValueError:
                 f_out.write('\n')
                 continue
 
-            try:
-                pred_token, pred_tag, confidence = tagged_line.split('__')
-                confidence = float(confidence)
-            except ValueError:
-                pred_token, pred_tag = tagged_line.split('__')
+            tagged_line =  tagged_line.split('__')
+            pred_tag = 'O'
 
-            assert token == pred_token
-
-            if confidence is None or confidence < lowest_confidence_allowed:
-                pred_tag = 'O'
+            if len(tagged_line) >= 3:
+                confidence = float(tagged_line[-1])
+                if confidence > confidence_threshold:
+                    pred_tag = tagged_line[-2]
 
             f_out.write('{} {} {} {}\n'.format(token, pos_tag, true_tag, pred_tag))
 
